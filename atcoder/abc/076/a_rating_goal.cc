3f52096171f345c0a5e2c75d651767caeb5db38a
bam1279 <155091565+bam1279@users.noreply.github.com>
2024-01-06 15:59:09 +0900

    feat(atcoder): add solution for ABC076 A - Rating Goal

---
+++ atcoder/abc/076/a_rating_goal.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  int r;
+  int g;
+  std::cin >> r >> g;
+
+  const int next_perf = 2 * g - r;
+  std::cout << next_perf << '\n';
+}

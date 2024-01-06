7c978ce92a14c876f32fb5d8f0e26532aacf48cd
bam1279 <155091565+bam1279@users.noreply.github.com>
2024-01-06 15:59:15 +0900

    refactor(atcoder): improve variable names in ABC084 A - New Year

--- atcoder/abc/084/a_new_year.cc
+++ atcoder/abc/084/a_new_year.cc
@@ -6,6 +6,7 @@ int main() {
   int m;
   std::cin >> m;
 
-  constexpr int HOURS_IN_A_DAY = 24;
-  std::cout << HOURS_IN_A_DAY + HOURS_IN_A_DAY - m << '\n';
+  constexpr int kHoursPerDay = 24;
+  const int hours_left = kHoursPerDay + kHoursPerDay - m;
+  std::cout << hours_left << '\n';
 }

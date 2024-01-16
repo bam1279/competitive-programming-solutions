75a67f4d0525873f9383fa4599b0f2ebdaee6191
bam1279 <155091565+bam1279@users.noreply.github.com>
2024-01-16 23:48:35 +0900

    feat(atcoder): add solution for ABC081 B - Shift only

---
+++ atcoder/abc/081/b_shift_only.cc
@@ -0,0 +1,51 @@
+/*
+ Copyright (c) 2023-2024 bam1279
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+#include <algorithm>
+#include <exception>
+#include <iostream>
+#include <vector>
+
+bool AreAllEven(const std::vector<int>& arr) {
+  return std::all_of(arr.begin(), arr.end(), [](int x) { return x % 2 == 0; });
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  try {
+    int n;
+    std::cin >> n;
+    std::vector<int> numbers(n);
+    for (int i = 0; i < n; ++i) std::cin >> numbers[i];
+
+    int operations_count = 0;
+    while (AreAllEven(numbers)) {
+      for (int i = 0; i < n; ++i) numbers[i] /= 2;
+      operations_count++;
+    }
+
+    std::cout << operations_count << '\n';
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+  }
+}

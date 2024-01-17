73797de253e0aeecbb8baee34fcf329b5065762e
bam1279 <155091565+bam1279@users.noreply.github.com>
2024-01-17 18:40:47 +0900

    feat(atcoder): add solution for ABC049 A - UOIAUAI

---
+++ atcoder/abc/049/a_uoiauai.cc
@@ -0,0 +1,39 @@
+/*
+ Copyright (c) 2023-2024 bam1279
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+#include <algorithm>
+#include <array>
+#include <iostream>
+
+bool IsVowel(char c) {
+  constexpr std::array<char, 5> kVowels = {'a', 'e', 'i', 'o', 'u'};
+  return std::find(kVowels.begin(), kVowels.end(), c) != std::end(kVowels);
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  char c;
+  std::cin >> c;
+
+  std::cout << (IsVowel(c) ? "vowel" : "consonant") << '\n';
+}

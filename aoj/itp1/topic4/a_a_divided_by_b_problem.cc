9ac43e127e1d6e13f751744971d65a2f658bf545
bam1279 <155091565+bam1279@users.noreply.github.com>
2024-02-14 19:20:20 +0900

    feat(aoj): add solution for ITP_4_A A/B Problem

---
+++ aoj/itp1/topic4/a_a_divided_by_b_problem.cc
@@ -0,0 +1,39 @@
+/*
+ Copyright (c) 2024 bam1279
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+#include <iomanip>
+#include <iostream>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  int a, b;
+  std::cin >> a >> b;
+
+  const int d = a / b;
+  const int r = a % b;
+  const double f = static_cast<double>(a) / b;
+
+  const int kPrecision = 5;
+  std::cout << std::fixed << std::setprecision(kPrecision) << d << ' ' << r
+            << ' ' << f << '\n';
+}

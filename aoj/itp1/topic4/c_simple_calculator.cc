e4266e287a6f5648ec1eeaca1bd8c454356f435f
bam1279 <155091565+bam1279@users.noreply.github.com>
2024-02-18 09:08:38 +0900

    feat(aoj): add solution for ITP1_4_C Simple Calculator

---
+++ aoj/itp1/topic4/c_simple_calculator.cc
@@ -0,0 +1,68 @@
+/*
+ Copyright (c) 2024 bam1279
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+#include <iostream>
+
+enum class Operator { Plus, Minus, Multiply, Divide };
+
+Operator CharToOperator(char c) {
+  switch (c) {
+    case '+':
+      return Operator::Plus;
+    case '-':
+      return Operator::Minus;
+    case '*':
+      return Operator::Multiply;
+    case '/':
+    default:
+      return Operator::Divide;
+  }
+}
+
+int Calc(int a, Operator op, int b) {
+  switch (op) {
+    case Operator::Plus:
+      return a + b;
+    case Operator::Minus:
+      return a - b;
+    case Operator::Multiply:
+      return a * b;
+    case Operator::Divide:
+    default:
+      return a / b;
+  }
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int a, b;
+  char op;
+  while (true) {
+    std::cin >> a >> op >> b;
+    if (op == '?') return 0;
+
+    const Operator o = CharToOperator(op);
+    std::cout << Calc(a, o, b) << '\n';
+  }
+}
